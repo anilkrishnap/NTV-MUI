9c25c42504cbc785ccc5262aad41ebc03f1550d3
anilkrishnap <anil.pulijala@radiant.digital>
2024-06-04 19:55:05 +0530

    KPI changes.

--- src/app/KPI/page.tsx
+++ src/app/KPI/page.tsx
@@ -1,11 +1,33 @@
 'use client';
-import React from 'react'
-import MUIAccordionComponent from '../../components/MUIAccordionComponent/MUIAccordionComponent';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MUICardComponent from '../../components/MUICardComponent/MUICardComponent';
+import KPIDataCardComponent from '../../components/KPIDataCardComponent/KPIDataCardComponent';
 
-function page() {
+
+export default function MUIAccordionComponent() {
   return (
-    <MUIAccordionComponent/>
-  )
+    <>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography><strong>KPI</strong></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Box sx={{ marginBottom: "16px" }}>
+            <MUICardComponent />
+          </Box>
+          <KPIDataCardComponent />
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
 }
-
-export default page
