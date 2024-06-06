e50034ee8d767dfed20a831d49b84ecb4c009355
Sannith <sannithpeddolla143@gmail.com>
2024-06-06 11:10:20 +0530

    removed coponents and added  other variant

--- src/app/NtvSideNav/page.tsx
+++ src/app/NtvSideNav/page.tsx
@@ -24,7 +24,7 @@ export default function MyComponent() {
       backgroundColor: 'var(--Dark-bg-bg1-value)',
       // backgroundColor: 'black',
       color:"white",
-      color2:"var(--DQI-Widget-sub-title, #98A2B3)",
+      // color2:"var(--DQI-Widget-sub-title, #98A2B3)",
     
       // backgroundColor:"black",
       backgroundColorCard:'var(--BG-2-blue-in-light, #152130)',
@@ -33,6 +33,20 @@ export default function MyComponent() {
       backgroundColorMadeline:"var(--BG-2-blue-in-light, #152130);"
       
       
+    },
+    secondary:{
+      backgroundColor: ' var(--Dark-general-g2-value)',
+      // backgroundColor: 'black',
+      color:" --Dark-text-t1-value",
+      // color2:"var(--DQI-Widget-sub-title, #98A2B3)",
+    
+      // backgroundColor:"black",
+      backgroundColorCard:'var( --Dark-general-g4-value)',
+      backgroundColorTopology:"var(--Dark-general-g2-value)",
+      backgroundColorHeader:"var(--Dark-general-g2-value);",
+      backgroundColorMadeline:"var(--Dark-general-g4-value);"
+      
+
     }
    
   }
@@ -55,9 +69,9 @@ export default function MyComponent() {
       }}
     >
       {/* <Header/> */}
-      <CustomSearchInput  variant={variants.primary}/>
-      <MadelineCompnent  variant={variants.primary} />
-      <MUIAccordionComponent  variant={variants.primary}  />
+      <CustomSearchInput />
+      <MadelineCompnent  />
+      <MUIAccordionComponent />
       {/* <Equipment  /> */}
     </Box>
       
